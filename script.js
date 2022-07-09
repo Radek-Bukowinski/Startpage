405674fd1d731ed36096837ddea203ba738564bb
Radek Bukowinski <radekbukowinski@protonmail.com>
2022-07-09 14:34:45 +0100

    Added video background and time

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+// Set video speed
+document.querySelector('video').playbackRate = 1.25;
+
+// Get the time and update the relevant HTML element
+/*
 function setTime() {
     var today = new Date();
+    var nameOfDay = today.getDay();
+    var month = today.getMonth();
+    var year = today.getFullYear();
     var hour = today.getHours();
     var minute = today.getMinutes();
     if(minute >= 0 && minute <= 9){
@@ -10,5 +18,13 @@ function setTime() {
     var time = hour + ":" + minute;
     document.getElementById("timeInner").innerHTML = time;
 }
+
+// Update the time every second
 setTimeout(setTime, 1000);
 //setInterval(setTime, 1000);
+*/
+
+setInterval( () => {
+    document.getElementById("timeInner").innerHTML = new Date().toLocaleString();
+}, 1000);
+
